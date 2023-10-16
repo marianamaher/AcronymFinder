78c07aa0ca39ca932cfb9b09cb09c483437f1686
Maher,Mariana <mariana_maher@vanguard.com>
2023-10-16 17:13:34 +0000

    initial commit

---
+++ scripts/content.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+
+function createExtensionBody(){
+
+    let container = $("<div class='container'></div>").css({
+        width: "343px",
+        height: "187px",
+        background: "#FFFFFF",
+        position: "fixed"
+    });
+    $(body).append(container);
+    
+}
+
+
+
+})
